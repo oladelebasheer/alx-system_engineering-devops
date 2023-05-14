5b4a95e752a6b1a708b997bc2b5debcb3f7f11c3
oladelebasheer <ajaoooladele@gmail.com>
2023-05-14 20:14:15 +0100

    Task update

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+# Fixing find why out why Apache is returning a 500 error
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
